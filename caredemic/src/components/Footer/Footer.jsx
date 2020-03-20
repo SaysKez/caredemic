03ee6e2c463bba5d4bd157ba535d169ad84004d9
Kezie Todd <kezie.todd@kezies-mbp.home>
2020-03-20 00:41:44 +0000

    trying (and failing) to get footer component to work

--- caredemic/src/components/Footer/Footer.jsx
+++ caredemic/src/components/Footer/Footer.jsx
@@ -8,11 +8,6 @@ class Footer extends Component {
     return (
         <div>
             <img className="footer-logo" src={logo} />
-            <Header as="h4" textAlign="center">
-              COVID-19 is sweeping the globe. But we are not helpless.
-              <br />
-              Fear is a virus. Be the cure.
-            </Header>
         </div>
         
     );
