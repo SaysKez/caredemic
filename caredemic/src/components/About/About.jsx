05a58b2ca732f95ec4ff99537c6c99dc37483252
Kezie Todd <me-and-i@myself.com>
2020-03-21 10:18:02 +0000

    added and formatted about page content

--- caredemic/src/components/About/About.jsx
+++ caredemic/src/components/About/About.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Header } from 'semantic-ui-react';
 import logo from './logo.png';
+import './about.css';
 
 class About extends Component {
 
@@ -8,51 +9,31 @@ class About extends Component {
     return (
         <div>
             <img alt="caredemic logo" className="heading-logo" src={logo} />
+            <Header as="h1" textAlign="center">The World is Our Neighbour</Header>
+            <section className="about">
+                <p><b>SPOILER ALERT:</b> There does not have to be a global pandemic to wash your hands (or help others).</p>
+
+                <p>Most of the ideas here could be done any time. Start now, don't stop. Make 2020 a year when hope and love spread farther than any virus can, and last much longer.</p>
+            </section>
+            
             <Header as="h1" textAlign="center">Why Caredemic?</Header>
-            <Header as="h4" textAlign="left">
-                He answered, “‘Love the Lord your God with all your heart and with all your soul and with all your strength and with all your mind’;
-                <br />
-                and, ‘Love your neighbor as yourself.’”
-                <br />
-                “You have answered correctly,” Jesus replied. “Do this and you will live.”
-                <br />
-                But he wanted to justify himself, so he asked Jesus, “And who is my neighbor?”
-                <br />
-                <br />
-                In reply Jesus said: “A man was going down from Jerusalem to Jericho, when he was attacked by robbers. 
-                <br />
-                They stripped him of his clothes, beat him and went away, leaving him half dead. 
-                <br />
-                A priest happened to be going down the same road, and when he saw the man, 
-                <br />
-                he passed by on the other side. 
-                <br />
-                So too, a Levite, when he came to the place and saw him, 
-                <br />
-                passed by on the other side. 
-                <br />
-                <br />
-                But a Samaritan, as he traveled, came where the man was; 
-                <br />
-                and when he saw him, he took pity on him. 
-                <br />
-                He went to him and bandaged his wounds, pouring on oil and wine. 
-                <br />
-                Then he put the man on his own donkey, brought him to an inn and took care of him. 
-                <br />
-                The next day he took out two denarii and gave them to the innkeeper. 
-                <br />
-                ‘Look after him,’ he said, ‘and when I return, I will reimburse you for any extra expense you may have.’
-                <br />
-                <br />
-                “Which of these three do you think was a neighbor to the man who fell into the hands of robbers?”
-                <br />
-                The expert in the law replied, “The one who had mercy on him.”
-                <br />
-                Jesus told him, “Go and do likewise.”
-                <br />
-                -Luke 10:27-37
-            </Header>
+            <section className="about">
+                <p>He answered, “‘Love the Lord your God with all your heart and with all your soul and with all your strength and with all your mind’; and, ‘Love your neighbor as yourself.’”</p>
+
+                <p>“You have answered correctly,” Jesus replied. “Do this and you will live.”</p>
+                
+                <p>But he wanted to justify himself, so he asked Jesus, “And who is my neighbor?”</p>
+
+                <p>In reply Jesus said: “A man was going down from Jerusalem to Jericho, when he was attacked by robbers. They stripped him of his clothes, beat him and went away, leaving him half dead. A priest happened to be going down the same road, and when he saw the man, he passed by on the other side. So too, a Levite, when he came to the place and saw him, passed by on the other side. But a Samaritan, as he traveled, came where the man was; and when he saw him, he took pity on him. He went to him and bandaged his wounds, pouring on oil and wine. Then he put the man on his own donkey, brought him to an inn and took care of him. The next day he took out two denarii and gave them to the innkeeper. ‘Look after him,’ he said, ‘and when I return, I will reimburse you for any extra expense you may have.’</p>
+
+                <p>“Which of these three do you think was a neighbor to the man who fell into the hands of robbers?”</p>
+
+                <p>The expert in the law replied, “The one who had mercy on him.”</p>
+                
+                <p>Jesus told him, “Go and do likewise.”</p>
+                
+                <cite>-Luke 10:27-37</cite>
+            </section>
         </div>
         
     );
